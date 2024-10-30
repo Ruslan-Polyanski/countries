aef3b31ca0e3ac6772d0a9b86aa6e888eb767337
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-10-30 19:56:38 +0300

    feat: add spinner from MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react';
 import { useQuery } from '@apollo/client';
 import { ALL_COUNTRIES } from '../apollo/getDataCountries';
+import { Spinner } from '../shared/spinner/Spinner';
 
 const App: FC = () => {
   const { loading, error, data } = useQuery(ALL_COUNTRIES);
   console.log(data)
+
+  if(loading) {
+    return <>
+      <Spinner />
+    </>;
+  }
+
   return (
     <div></div>
   );
