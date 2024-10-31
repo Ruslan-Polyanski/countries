46c9fa5a832f0307d86862d2fa98169d73ad6e36
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-10-31 22:09:24 +0300

    refactor: delete function get flags from emoji

--- src/shared/cardCountry/CardCountry.tsx
+++ src/shared/cardCountry/CardCountry.tsx
@@ -4,14 +4,11 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { ICountryData } from '../types/global';
-import { getFlagToEmoji } from '../utils/getFlagToEmoji';
 
 const CardCountry: FC<ICountryData> = ({emoji, capital, currency, continent, name, phone}) => {
- const flag = getFlagToEmoji(emoji);
 
   return (
       <Card sx={{ maxWidth: 300, minWidth: 200 }}>
-        <div>{flag}</div>
         <div>{emoji}</div>
         <div>{capital}</div>
         <div>{currency}</div>
